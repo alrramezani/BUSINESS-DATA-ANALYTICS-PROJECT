31bf2f2eecd0315cd0785e007719e0e9adcf80dc
alr.ramezani <alr.ramezani@gmail.com>
2025-04-09 09:25:07 +0200

    Data Preprocessing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+
 
 # Load the telecom_churn dataset from a CSV file located in the './data/' directory
 df = pd.read_csv('./data/telecom_churn.csv')
@@ -8,3 +11,24 @@ print(df.head())
 print(df.info())
 # Generate descriptive statistics for the DataFrame, such as mean, std, min, max, etc.
 print(df.describe())
+
+# Handle missing values (example)
+df.replace(" ", np.nan, inplace=True)
+df.dropna(inplace=True)
+
+# Encode categorical features
+for col in df.select_dtypes(include=['object']).columns:
+    if col != 'Churn':
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+
+# Convert target variable
+df['Churn'] = df['Churn'].map({'No': 0, 'Yes': 1})
+
+# Normalize numerical features
+scaler = StandardScaler()
+num_cols = df.select_dtypes(include=['int64', 'float64']).columns.drop('Churn')
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+# Print DataFrame information to verify changes and ensure data integrity
+print(df.info())
