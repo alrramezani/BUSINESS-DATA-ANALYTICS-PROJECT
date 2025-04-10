7a468ed369cc86bb30fef500bdb14d1058041f93
alr.ramezani <alr.ramezani@gmail.com>
2025-04-10 09:43:20 +0200

    clean data maker method

---
+++ src/clean_data_maker.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+
+def make_file():
+    # Load the telecom_churn dataset from a CSV file located in the './data/' directory
+    df = pd.read_csv('./data/telecom_churn.csv')
+    # Display the first few rows of the DataFrame to get an initial view of the data
+    print(df.head())
+    # Print information about the DataFrame, including data types and missing values
+    print(df.info())
+    # Generate descriptive statistics for the DataFrame, such as mean, std, min, max, etc.
+    print(df.describe())
+
+    # Handle missing values (example)
+    df.replace(" ", np.nan, inplace=True)
+    df.dropna(inplace=True)
+
+    # Encode categorical features
+    for col in df.select_dtypes(include=['object']).columns:
+        if col != 'Churn':
+            le = LabelEncoder()
+            df[col] = le.fit_transform(df[col])
+
+    # Convert target variable
+    df['Churn'] = df['Churn'].map({'No': 0, 'Yes': 1})
+
+    # Normalize numerical features
+    scaler = StandardScaler()
+    num_cols = df.select_dtypes(include=['int64', 'float64']).columns.drop('Churn')
+    df[num_cols] = scaler.fit_transform(df[num_cols])
+
+    # Print DataFrame information to verify changes and ensure data integrity
+    print(df.info())
+    # Save the cleaned data to a file for future analysis or reuse.
+    df.to_csv('./data/clean_data.csv', index=False)
--- src/main.py
+++ src/main.py
@@ -1,39 +1,14 @@
+import os
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import StandardScaler,LabelEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
+import clean_data_maker
 
-# Load the telecom_churn dataset from a CSV file located in the './data/' directory
-df = pd.read_csv('./data/telecom_churn.csv')
-# Display the first few rows of the DataFrame to get an initial view of the data
-print(df.head())
-# Print information about the DataFrame, including data types and missing values
-print(df.info())
-# Generate descriptive statistics for the DataFrame, such as mean, std, min, max, etc.
-print(df.describe())
-
-# Handle missing values (example)
-df.replace(" ", np.nan, inplace=True)
-df.dropna(inplace=True)
-
-# Encode categorical features
-for col in df.select_dtypes(include=['object']).columns:
-    if col != 'Churn':
-        le = LabelEncoder()
-        df[col] = le.fit_transform(df[col])
-
-# Convert target variable
-df['Churn'] = df['Churn'].map({'No': 0, 'Yes': 1})
-
-# Normalize numerical features
-scaler = StandardScaler()
-num_cols = df.select_dtypes(include=['int64', 'float64']).columns.drop('Churn')
-df[num_cols] = scaler.fit_transform(df[num_cols])
-
-# Print DataFrame information to verify changes and ensure data integrity
-print(df.info())
-
+# Check if the CSV file exists and load the cleaned dataset from './data/'
+if not os.path.exists("./data/clean_data.csv"):
+    clean_data_maker.make_file()
+df = pd.read_csv("./data/clean_data.csv")
 
 # Churn distribution
 sns.countplot(x='Churn', data=df)
@@ -44,3 +19,4 @@ plt.figure(figsize=(12, 8))
 sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
 plt.title("Feature Correlation")
 plt.savefig("./figs/feature_correlation")
+
